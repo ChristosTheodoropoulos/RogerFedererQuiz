80935a2d02bf811bc93826cf8ab8e5ce7913e4ea
ChristosTheodoropoulos <christosth.rf@gmail.com>
2017-04-02 17:21:26 +0300

    8th Commit: Java Improvements

--- app/src/main/java/com/example/android/rogerfedererquiz/MainActivity.java
+++ app/src/main/java/com/example/android/rogerfedererquiz/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import static android.widget.Toast.makeText;
@@ -21,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     String nameOfPlayer;
 
     // Question 1 Views
+    RadioGroup question1RadioGroup;
     RadioButton question1RadioButton1;
     RadioButton question1RadioButton2;
     RadioButton question1RadioButton3;
@@ -33,12 +35,14 @@ public class MainActivity extends AppCompatActivity {
     CheckBox question2CheckBox4;
 
     // Question 3 Views
+    RadioGroup question3RadioGroup;
     RadioButton question3RadioButton1;
     RadioButton question3RadioButton2;
     RadioButton question3RadioButton3;
     RadioButton question3RadioButton4;
 
     // Question 4 Views
+    RadioGroup question4RadioGroup;
     RadioButton question4RadioButton1;
     RadioButton question4RadioButton2;
     RadioButton question4RadioButton3;
@@ -51,12 +55,14 @@ public class MainActivity extends AppCompatActivity {
     CheckBox question5CheckBox4;
 
     // Question 6 Views
+    RadioGroup question6RadioGroup;
     RadioButton question6RadioButton1;
     RadioButton question6RadioButton2;
     RadioButton question6RadioButton3;
     RadioButton question6RadioButton4;
 
     // Question 7 Views
+    RadioGroup question7RadioGroup;
     RadioButton question7RadioButton1;
     RadioButton question7RadioButton2;
     RadioButton question7RadioButton3;
@@ -70,40 +76,25 @@ public class MainActivity extends AppCompatActivity {
     // Global variables
     int correctAnswers = 0;
 
-    boolean question1RadioButton1Checked;
-    boolean question1RadioButton2Checked;
-    boolean question1RadioButton3Checked;
-    boolean question1RadioButton4Checked;
+    int question1RadioButtonSelectedId;
 
     boolean question2CheckBox1Checked;
     boolean question2CheckBox2Checked;
     boolean question2CheckBox3Checked;
     boolean question2CheckBox4Checked;
 
-    boolean question3RadioButton1Checked;
-    boolean question3RadioButton2Checked;
-    boolean question3RadioButton3Checked;
-    boolean question3RadioButton4Checked;
+    int question3RadioButtonSelectedId;
 
-    boolean question4RadioButton1Checked;
-    boolean question4RadioButton2Checked;
-    boolean question4RadioButton3Checked;
-    boolean question4RadioButton4Checked;
+    int question4RadioButtonSelectedId;
 
     boolean question5CheckBox1Checked;
     boolean question5CheckBox2Checked;
     boolean question5CheckBox3Checked;
     boolean question5CheckBox4Checked;
 
-    boolean question6RadioButton1Checked;
-    boolean question6RadioButton2Checked;
-    boolean question6RadioButton3Checked;
-    boolean question6RadioButton4Checked;
+    int question6RadioButtonSelectedId;
 
-    boolean question7RadioButton1Checked;
-    boolean question7RadioButton2Checked;
-    boolean question7RadioButton3Checked;
-    boolean question7RadioButton4Checked;
+    int question7RadioButtonSelectedId;
 
     String answerQuestion8;
     String answerQuestion9;
@@ -125,40 +116,25 @@ public class MainActivity extends AppCompatActivity {
         // Save custom values into the bundle.
         savedInstanceState.putString("name", nameOfPlayer);
 
-        savedInstanceState.putBoolean("isQuestion1RadioButton1Checked", question1RadioButton1Checked);
-        savedInstanceState.putBoolean("isQuestion1RadioButton2Checked", question1RadioButton2Checked);
-        savedInstanceState.putBoolean("isQuestion1RadioButton3Checked", question1RadioButton3Checked);
-        savedInstanceState.putBoolean("isQuestion1RadioButton4Checked", question1RadioButton4Checked);
+        savedInstanceState.putInt("question1RadioButtonSelectedId", question1RadioButtonSelectedId);
 
         savedInstanceState.putBoolean("isQuestion2CheckBox1Checked", question2CheckBox1Checked);
         savedInstanceState.putBoolean("isQuestion2CheckBox2Checked", question2CheckBox2Checked);
         savedInstanceState.putBoolean("isQuestion2CheckBox3Checked", question2CheckBox3Checked);
         savedInstanceState.putBoolean("isQuestion2CheckBox4Checked", question2CheckBox4Checked);
 
-        savedInstanceState.putBoolean("isQuestion3RadioButton1Checked", question3RadioButton1Checked);
-        savedInstanceState.putBoolean("isQuestion3RadioButton2Checked", question3RadioButton2Checked);
-        savedInstanceState.putBoolean("isQuestion3RadioButton3Checked", question3RadioButton3Checked);
-        savedInstanceState.putBoolean("isQuestion3RadioButton4Checked", question3RadioButton4Checked);
+        savedInstanceState.putInt("question3RadioButtonSelectedId", question3RadioButtonSelectedId);
 
-        savedInstanceState.putBoolean("isQuestion4RadioButton1Checked", question4RadioButton1Checked);
-        savedInstanceState.putBoolean("isQuestion4RadioButton2Checked", question4RadioButton2Checked);
-        savedInstanceState.putBoolean("isQuestion4RadioButton3Checked", question4RadioButton3Checked);
-        savedInstanceState.putBoolean("isQuestion4RadioButton4Checked", question4RadioButton4Checked);
+        savedInstanceState.putInt("question4RadioButtonSelectedId", question4RadioButtonSelectedId);
 
         savedInstanceState.putBoolean("isQuestion5CheckBox1Checked", question5CheckBox1Checked);
         savedInstanceState.putBoolean("isQuestion5CheckBox2Checked", question5CheckBox2Checked);
         savedInstanceState.putBoolean("isQuestion5CheckBox3Checked", question5CheckBox3Checked);
         savedInstanceState.putBoolean("isQuestion5CheckBox4Checked", question5CheckBox4Checked);
 
-        savedInstanceState.putBoolean("isQuestion6RadioButton1Checked", question6RadioButton1Checked);
-        savedInstanceState.putBoolean("isQuestion6RadioButton2Checked", question6RadioButton2Checked);
-        savedInstanceState.putBoolean("isQuestion6RadioButton3Checked", question6RadioButton3Checked);
-        savedInstanceState.putBoolean("isQuestion6RadioButton4Checked", question6RadioButton4Checked);
+        savedInstanceState.putInt("question6RadioButtonSelectedId", question6RadioButtonSelectedId);
 
-        savedInstanceState.putBoolean("isQuestion7RadioButton1Checked", question7RadioButton1Checked);
-        savedInstanceState.putBoolean("isQuestion7RadioButton2Checked", question7RadioButton2Checked);
-        savedInstanceState.putBoolean("isQuestion7RadioButton3Checked", question7RadioButton3Checked);
-        savedInstanceState.putBoolean("isQuestion7RadioButton4Checked", question7RadioButton4Checked);
+        savedInstanceState.putInt("question7RadioButtonSelectedId", question7RadioButtonSelectedId);
 
         savedInstanceState.putString("answerQuestion8", answerQuestion8);
         savedInstanceState.putString("answerQuestion9", answerQuestion9);
@@ -180,6 +156,7 @@ public class MainActivity extends AppCompatActivity {
          */
         nameEditText = (EditText) findViewById(R.id.name_edit_text);
 
+        question1RadioGroup = (RadioGroup) findViewById(R.id.question_1_radio_group);
         question1RadioButton1 = (RadioButton) findViewById(R.id.question_1_radio_button_1);
         question1RadioButton2 = (RadioButton) findViewById(R.id.question_1_radio_button_2);
         question1RadioButton3 = (RadioButton) findViewById(R.id.question_1_radio_button_3);
@@ -190,11 +167,13 @@ public class MainActivity extends AppCompatActivity {
         question2CheckBox3 = (CheckBox) findViewById(R.id.question_2_check_box_3);
         question2CheckBox4 = (CheckBox) findViewById(R.id.question_2_check_box_4);
 
+        question3RadioGroup = (RadioGroup) findViewById(R.id.question_3_radio_group);
         question3RadioButton1 = (RadioButton) findViewById(R.id.question_3_radio_button_1);
         question3RadioButton2 = (RadioButton) findViewById(R.id.question_3_radio_button_2);
         question3RadioButton3 = (RadioButton) findViewById(R.id.question_3_radio_button_3);
         question3RadioButton4 = (RadioButton) findViewById(R.id.question_3_radio_button_4);
 
+        question4RadioGroup = (RadioGroup) findViewById(R.id.question_4_radio_group);
         question4RadioButton1 = (RadioButton) findViewById(R.id.question_4_radio_button_1);
         question4RadioButton2 = (RadioButton) findViewById(R.id.question_4_radio_button_2);
         question4RadioButton3 = (RadioButton) findViewById(R.id.question_4_radio_button_3);
@@ -205,11 +184,13 @@ public class MainActivity extends AppCompatActivity {
         question5CheckBox3 = (CheckBox) findViewById(R.id.question_5_check_box_3);
         question5CheckBox4 = (CheckBox) findViewById(R.id.question_5_check_box_4);
 
+        question6RadioGroup = (RadioGroup) findViewById(R.id.question_6_radio_group);
         question6RadioButton1 = (RadioButton) findViewById(R.id.question_6_radio_button_1);
         question6RadioButton2 = (RadioButton) findViewById(R.id.question_6_radio_button_2);
         question6RadioButton3 = (RadioButton) findViewById(R.id.question_6_radio_button_3);
         question6RadioButton4 = (RadioButton) findViewById(R.id.question_6_radio_button_4);
 
+        question7RadioGroup = (RadioGroup) findViewById(R.id.question_7_radio_group);
         question7RadioButton1 = (RadioButton) findViewById(R.id.question_7_radio_button_1);
         question7RadioButton2 = (RadioButton) findViewById(R.id.question_7_radio_button_2);
         question7RadioButton3 = (RadioButton) findViewById(R.id.question_7_radio_button_3);
@@ -222,40 +203,25 @@ public class MainActivity extends AppCompatActivity {
         if (savedInstanceState != null) {
             nameOfPlayer = savedInstanceState.getString("name");
 
-            question1RadioButton1Checked = savedInstanceState.getBoolean("isQuestion1RadioButton1Checked");
-            question1RadioButton2Checked = savedInstanceState.getBoolean("isQuestion1RadioButton2Checked");
-            question1RadioButton3Checked = savedInstanceState.getBoolean("isQuestion1RadioButton3Checked");
-            question1RadioButton4Checked = savedInstanceState.getBoolean("isQuestion1RadioButton4Checked");
+            question1RadioButtonSelectedId = savedInstanceState.getInt("question1RadioButtonSelectedId");
 
             question2CheckBox1Checked = savedInstanceState.getBoolean("isQuestion2CheckBox1Checked");
             question2CheckBox2Checked = savedInstanceState.getBoolean("isQuestion2CheckBox2Checked");
             question2CheckBox3Checked = savedInstanceState.getBoolean("isQuestion2CheckBox3Checked");
             question2CheckBox4Checked = savedInstanceState.getBoolean("isQuestion2CheckBox4Checked");
 
-            question3RadioButton1Checked = savedInstanceState.getBoolean("isQuestion3RadioButton1Checked");
-            question3RadioButton2Checked = savedInstanceState.getBoolean("isQuestion3RadioButton2Checked");
-            question3RadioButton3Checked = savedInstanceState.getBoolean("isQuestion3RadioButton3Checked");
-            question3RadioButton4Checked = savedInstanceState.getBoolean("isQuestion3RadioButton4Checked");
+            question3RadioButtonSelectedId = savedInstanceState.getInt("question3RadioButtonSelectedId");
 
-            question4RadioButton1Checked = savedInstanceState.getBoolean("isQuestion4RadioButton1Checked");
-            question4RadioButton2Checked = savedInstanceState.getBoolean("isQuestion4RadioButton2Checked");
-            question4RadioButton3Checked = savedInstanceState.getBoolean("isQuestion4RadioButton3Checked");
-            question4RadioButton4Checked = savedInstanceState.getBoolean("isQuestion4RadioButton4Checked");
+            question4RadioButtonSelectedId = savedInstanceState.getInt("question4RadioButtonSelectedId");
 
             question5CheckBox1Checked = savedInstanceState.getBoolean("isQuestion5CheckBox1Checked");
             question5CheckBox2Checked = savedInstanceState.getBoolean("isQuestion5CheckBox2Checked");
             question5CheckBox3Checked = savedInstanceState.getBoolean("isQuestion5CheckBox3Checked");
             question5CheckBox4Checked = savedInstanceState.getBoolean("isQuestion5CheckBox4Checked");
 
-            question6RadioButton1Checked = savedInstanceState.getBoolean("isQuestion6RadioButton1Checked");
-            question6RadioButton2Checked = savedInstanceState.getBoolean("isQuestion6RadioButton2Checked");
-            question6RadioButton3Checked = savedInstanceState.getBoolean("isQuestion6RadioButton3Checked");
-            question6RadioButton4Checked = savedInstanceState.getBoolean("isQuestion6RadioButton4Checked");
+            question6RadioButtonSelectedId = savedInstanceState.getInt("question6RadioButtonSelectedId");
 
-            question7RadioButton1Checked = savedInstanceState.getBoolean("isQuestion7RadioButton1Checked");
-            question7RadioButton2Checked = savedInstanceState.getBoolean("isQuestion7RadioButton2Checked");
-            question7RadioButton3Checked = savedInstanceState.getBoolean("isQuestion7RadioButton3Checked");
-            question7RadioButton4Checked = savedInstanceState.getBoolean("isQuestion7RadioButton4Checked");
+            question7RadioButtonSelectedId = savedInstanceState.getInt("question7RadioButtonSelectedId");
 
             answerQuestion8 = savedInstanceState.getString("answerQuestion8");
             answerQuestion9 = savedInstanceState.getString("answerQuestion9");
@@ -296,29 +262,11 @@ public class MainActivity extends AppCompatActivity {
      * @correctAnswers: global variable
      */
     public String calculateQuizResult(){
-        // Take the answer of question 1. Check which radio button is checked.
-        question1RadioButton1Checked = question1RadioButton1.isChecked();
-        question1RadioButton2Checked = question1RadioButton2.isChecked();
-        question1RadioButton3Checked = question1RadioButton3.isChecked();
-        question1RadioButton4Checked = question1RadioButton4.isChecked();
-
         // Take the answer of question 2. Check if any check box is checked.
         question2CheckBox1Checked = question2CheckBox1.isChecked();
         question2CheckBox2Checked = question2CheckBox2.isChecked();
         question2CheckBox3Checked = question2CheckBox3.isChecked();
         question2CheckBox4Checked = question2CheckBox4.isChecked();
-
-        // Take the answer of question 3. Check which radio button is checked.
-        question3RadioButton1Checked = question3RadioButton1.isChecked();
-        question3RadioButton2Checked = question3RadioButton2.isChecked();
-        question3RadioButton3Checked = question3RadioButton3.isChecked();
-        question3RadioButton4Checked = question3RadioButton4.isChecked();
-
-        // Take the answer of question 4. Check which radio button is checked.
-        question4RadioButton1Checked = question4RadioButton1.isChecked();
-        question4RadioButton2Checked = question4RadioButton2.isChecked();
-        question4RadioButton3Checked = question4RadioButton3.isChecked();
-        question4RadioButton4Checked = question4RadioButton4.isChecked();
 
         // Take the answer of question 5. Check if any check box is checked.
         question5CheckBox1Checked = question5CheckBox1.isChecked();
@@ -326,25 +274,14 @@ public class MainActivity extends AppCompatActivity {
         question5CheckBox3Checked = question5CheckBox3.isChecked();
         question5CheckBox4Checked = question5CheckBox4.isChecked();
 
-        // Take the answer of question 6. Check which radio button is checked.
-        question6RadioButton1Checked = question6RadioButton1.isChecked();
-        question6RadioButton2Checked = question6RadioButton2.isChecked();
-        question6RadioButton3Checked = question6RadioButton3.isChecked();
-        question6RadioButton4Checked = question6RadioButton4.isChecked();
-
-        // Take the answer of question 7. Check which radio button is checked.
-        question7RadioButton1Checked = question7RadioButton1.isChecked();
-        question7RadioButton2Checked = question7RadioButton2.isChecked();
-        question7RadioButton3Checked = question7RadioButton3.isChecked();
-        question7RadioButton4Checked = question7RadioButton4.isChecked();
-
         // Find the user's name.
         answerQuestion8 = questionAnswer8.getText().toString();
         answerQuestion9 = questionAnswer9.getText().toString();
         answerQuestion10 = questionAnswer10.getText().toString();
 
         // Check Question 1 if it's correct.
-        if (question1RadioButton3Checked == true){
+        question1RadioButtonSelectedId = question1RadioGroup.getCheckedRadioButtonId();
+        if (question1RadioGroup.findViewById(question1RadioButtonSelectedId) == question1RadioButton3) {
             correctAnswers += 1;
         }
 
@@ -355,12 +292,13 @@ public class MainActivity extends AppCompatActivity {
         }
 
         // Check Question 3 if it's correct.
-        if (question3RadioButton3Checked == true){
+        question3RadioButtonSelectedId = question3RadioGroup.getCheckedRadioButtonId();
+        if (question3RadioGroup.findViewById(question3RadioButtonSelectedId) == question3RadioButton3) {
             correctAnswers += 1;
         }
 
-        // Check Question 4 if it's correct.
-        if (question4RadioButton4Checked == true){
+        question4RadioButtonSelectedId = question4RadioGroup.getCheckedRadioButtonId();
+        if (question4RadioGroup.findViewById(question4RadioButtonSelectedId) == question4RadioButton4) {
             correctAnswers += 1;
         }
 
@@ -370,13 +308,14 @@ public class MainActivity extends AppCompatActivity {
             correctAnswers += 1;
         }
 
-        // Check Question 6 if it's correct.
-        if (question6RadioButton1Checked == true){
+        question6RadioButtonSelectedId = question6RadioGroup.getCheckedRadioButtonId();
+        if (question6RadioGroup.findViewById(question6RadioButtonSelectedId) == question6RadioButton1) {
             correctAnswers += 1;
         }
 
         // Check Question 7 if it's correct.
-        if (question7RadioButton4Checked == true){
+        question7RadioButtonSelectedId = question7RadioGroup.getCheckedRadioButtonId();
+        if (question7RadioGroup.findViewById(question7RadioButtonSelectedId) == question7RadioButton4) {
             correctAnswers += 1;
         }
 
